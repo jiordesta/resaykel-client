251846e65a0dc363d3fdb912fbdf50fbc0bbd194
Irson <jiordesta@gmail.com>
2024-03-20 13:59:34 +0800

    fix api calls

--- src/redux/axios_instance.js
+++ src/redux/axios_instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://resaykel-server.onrender.com",
   timeout: 50000,
   withCredentials: true,
 });
